49fdca251e9c1279cb70bf9fd6a080d64427469e
eddycheong <cheong.e49@gmail.com>
2017-10-31 20:48:31 +0000

    Refactored sample data to data.json instead of hardcoding in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,16 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 import Nav from './Nav.jsx';
 
+import data from '../data.json';
+// var json = require('../data.json');
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      currentUser: {name: "Bob"},
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      currentUser: data.currentUser,
+      messages: data.messages
     };
   }
 
