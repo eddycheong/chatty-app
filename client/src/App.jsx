ff8cbb54b178544772bfe116703098a45dc1209e
eddycheong <cheong.e49@gmail.com>
2017-11-01 19:50:03 +0000

    Moved the event listener from onMessage event handler to componentDidMount. Removed the setTimeout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,6 +25,22 @@ class App extends Component {
     };
 
     this.socket.send(JSON.stringify(message));
+  }
+
+  componentDidMount() {
+    // console.log("componentDidMount <App />");
+    // setTimeout(() => {
+    //   console.log("Simulating incoming message");
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = { id: 3, username: "Michelle", content: "Hello there!" };
+    //   const messages = this.state.messages.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({ messages: messages })
+    // }, 3000);
+
+    this.socket = new WebSocket('ws://localhost:3001');
+    console.log("Connected to server");
 
     this.socket.onmessage = (event) => {
       const messages = this.state.messages.concat(
@@ -35,22 +51,6 @@ class App extends Component {
         messages: messages
       });
     }
-  }
-
-  componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = { id: 3, username: "Michelle", content: "Hello there!" };
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({ messages: messages })
-    }, 3000);
-
-    this.socket = new WebSocket('ws://localhost:3001');
-    console.log("Connected to server");
   }
 
   render() {
